a7677821a099c56a66ed49433ac59f4f50254f38
Magno Vasquez <magno.vasquez@gmail.com>
2021-03-24 13:27:38 -0400

    Add project files.

---
+++ DynamicSQLConnection/SqlConnectionDynamic.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace DynamicSQLConnection
+{
+    partial class SqlConnectionDynamic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.cboServers = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboDB = new System.Windows.Forms.ComboBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnLoadServers = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(103, 184);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(98, 30);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test Connection";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // cboServers
+            // 
+            this.cboServers.FormattingEnabled = true;
+            this.cboServers.Location = new System.Drawing.Point(81, 28);
+            this.cboServers.Name = "cboServers";
+            this.cboServers.Size = new System.Drawing.Size(224, 21);
+            this.cboServers.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DB:";
+            // 
+            // cboDB
+            // 
+            this.cboDB.FormattingEnabled = true;
+            this.cboDB.Location = new System.Drawing.Point(81, 55);
+            this.cboDB.Name = "cboDB";
+            this.cboDB.Size = new System.Drawing.Size(224, 21);
+            this.cboDB.TabIndex = 3;
+            this.cboDB.DropDown += new System.EventHandler(this.cboDB_DropDown);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(81, 85);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(224, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(81, 111);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(224, 20);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Username:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(207, 184);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(98, 30);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnLoadServers
+            // 
+            this.btnLoadServers.Location = new System.Drawing.Point(311, 28);
+            this.btnLoadServers.Name = "btnLoadServers";
+            this.btnLoadServers.Size = new System.Drawing.Size(52, 23);
+            this.btnLoadServers.TabIndex = 0;
+            this.btnLoadServers.Text = "Refresh";
+            this.btnLoadServers.UseVisualStyleBackColor = true;
+            this.btnLoadServers.Click += new System.EventHandler(this.btnLoadServers_Click);
+            // 
+            // SqlConnectionDynamic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 226);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboDB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboServers);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnLoadServers);
+            this.Controls.Add(this.btnTest);
+            this.Name = "SqlConnectionDynamic";
+            this.Text = "SqlConnectionDynamic";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.ComboBox cboServers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboDB;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnLoadServers;
+    }
+}
---
+++ DynamicSQLConnection/SqlConnectionDynamic.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicSQLConnection
+{
+    public partial class SqlConnectionDynamic : Form
+    {
+        public SqlConnectionDynamic()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void LoadServers()
+        {
+            cboServers.Items.Clear();
+            //Adding local server in case you are using SQL SERVER Express locally.
+            cboServers.Items.Add("(localDB)\\MSSQLLocalDB");
+            
+            DataTable table = System.Data.Sql.SqlDataSourceEnumerator.Instance.GetDataSources();
+            
+            foreach (DataRow server in table.Rows)
+            {
+                cboServers.Items.Add(server[table.Columns["ServerName"]].ToString());
+            }
+        }
+
+        private void btnLoadServers_Click(object sender, EventArgs e)
+        {
+            LoadServers();
+        }
+
+        private void cboDB_DropDown(object sender, EventArgs e)
+        {
+            try
+            {
+                string server = cboServers.Text;
+
+                string connectionString = @"Data Source=" + server + "; User = "+txtUsername.Text.Trim()+"; Password="+txtPassword.Text.Trim()+";";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", con))
+                    {
+                        using (SqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            cboDB.Items.Clear();
+                            while (dr.Read())
+                            {
+                                cboDB.Items.Add(dr[0].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+               
+            }
+           
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=" + cboServers.Text + "; Initial Catalog = " + cboDB.Text + ";User = " + txtUsername.Text.Trim() + "; Password=" + txtPassword.Text.Trim() + ";";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                    MessageBox.Show("Connection succeful!");
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+               
+            }
+ 
+            
+           
+            
+        }
+    }
+}
